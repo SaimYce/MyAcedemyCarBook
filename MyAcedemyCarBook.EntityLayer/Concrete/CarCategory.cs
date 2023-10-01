11e16e57b4f8ff0dea6707ebdc5d462e836324e2
saim <Saim_yuce@outlook.com>
2023-10-01 12:33:33 +0300

    Proje dosyası ekle.

---
+++ MyAcedemyCarBook.EntityLayer/Concrete/CarCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcedemyCarBook.EntityLayer.Concrete
+{
+    public class CarCategory
+    {
+        public int CarCategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public List<Car> Cars { get; set; }
+
+    }
+}
---
+++ MyAcedemyCarBook.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAcedemyCarBook.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
